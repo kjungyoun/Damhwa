cfca429c7021a32f91f6691f0139b939f07339d0
minkpang <pmink210@gmail.com>
2021-09-23 11:32:29 +0900

    init_ios_pjt

---
+++ damhwa_client/DamHwa_Ios/DamHwa_Ios/DamHwa_IosApp.swift
@@ -0,0 +1,17 @@
+//
+//  DamHwa_IosApp.swift
+//  DamHwa_Ios
+//
+//  Created by minkpang on 2021/09/23.
+//
+
+import SwiftUI
+
+@main
+struct DamHwa_IosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
