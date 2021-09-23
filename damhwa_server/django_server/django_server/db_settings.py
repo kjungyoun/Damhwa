b6dda49120dafa8aeff2caa94ec2ef105cf2fce8
kjungyoun <rlawjddbs963@naver.com>
2021-09-23 14:53:46 +0900

    Django Project Setting

---
+++ damhwa_server/django_server/django_server/db_settings.py
@@ -0,0 +1,12 @@
+#db_settings
+DATABASES = {
+	'default' : {
+		'ENGINE' : 'django.db.backends.mysql',
+		'NAME' : 'damhwa',
+		'USER' : 'ssafy',
+		'PASSWORD' : 'ssafy',
+		'HOST' : 'localhost',
+		'PORT' : '3306',
+	}
+}
+SECRET_KEY = 'django-insecure-6=*xlb*5on8l#03m1r)9p16&psuk&kvzlj-k&*whr%=60uu_v@'
