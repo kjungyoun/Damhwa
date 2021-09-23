9c7875950dc7d172110dc7267d91af3edbb6bc84
kjungyoun <rlawjddbs963@naver.com>
2021-09-23 21:09:16 +0900

    Spring Config yaml

---
+++ damhwa_server/spring_server/src/main/java/com/ssafy/damhwa/api/controller/AuthController.java
@@ -0,0 +1,13 @@
+package com.ssafy.damhwa.api.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+
+}
