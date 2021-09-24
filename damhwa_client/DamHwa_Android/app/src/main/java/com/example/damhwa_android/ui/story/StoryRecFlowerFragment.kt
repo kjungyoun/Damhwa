dfa67a7bb1002502253fdc832669847b5d90e0b6
Huijiny <jiin20803@gmail.com>
2021-09-24 14:31:56 +0900

    Add navigation layout of storyRecommandedFlowerFragment

--- damhwa_client/DamHwa_Android/app/src/main/java/com/example/damhwa_android/ui/story/StoryRecFlowerFragment.kt
+++ damhwa_client/DamHwa_Android/app/src/main/java/com/example/damhwa_android/ui/story/StoryRecFlowerFragment.kt
@@ -1,9 +1,27 @@
 package com.example.damhwa_android.ui.story
 
+import androidx.navigation.fragment.findNavController
 import com.example.damhwa_android.R
 import com.example.damhwa_android.base.BaseFragment
 import com.example.damhwa_android.databinding.FragmentStoryRecFlowerBinding
 
 class StoryRecFlowerFragment : BaseFragment<FragmentStoryRecFlowerBinding>(
     R.layout.fragment_story_rec_flower
-)
+) {
+    override fun init() {
+        super.init()
+        binding.backButton.setOnClickListener {
+            routeToBack()
+        }
+        binding.navitest.setOnClickListener {
+            routeToFlowerDetail()
+        }
+    }
+
+    private fun routeToBack() =
+        findNavController().navigate(R.id.action_storyRecFlowerFragment_to_storyFragment)
+
+    private fun routeToFlowerDetail() =
+        findNavController().navigate(R.id.action_storyRecFlowerFragment_to_storyFlowerDetailFragment)
+
+}
