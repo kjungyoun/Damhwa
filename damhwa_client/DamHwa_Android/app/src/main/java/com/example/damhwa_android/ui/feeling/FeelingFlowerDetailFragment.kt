196e1609fa0933b891203716eccc289dd13a7dd6
Huijiny <jiin20803@gmail.com>
2021-09-24 14:31:33 +0900

    Add Feelingfragment spannable text and layout

--- damhwa_client/DamHwa_Android/app/src/main/java/com/example/damhwa_android/ui/feeling/FeelingFlowerDetailFragment.kt
+++ damhwa_client/DamHwa_Android/app/src/main/java/com/example/damhwa_android/ui/feeling/FeelingFlowerDetailFragment.kt
@@ -1,9 +1,80 @@
 package com.example.damhwa_android.ui.feeling
 
+import android.graphics.Typeface
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
+import android.text.style.StyleSpan
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
 import com.example.damhwa_android.R
 import com.example.damhwa_android.base.BaseFragment
-import com.example.damhwa_android.databinding.FragmentFeelingBinding
+import com.example.damhwa_android.databinding.FragmentFeelingFlowerDetailBinding
 
-class FeelingFlowerDetailFragment : BaseFragment<FragmentFeelingBinding>(
+class FeelingFlowerDetailFragment : BaseFragment<FragmentFeelingFlowerDetailBinding>(
     R.layout.fragment_feeling_flower_detail
-)
+) {
+    private var feeling: String = "조금 우울한"
+    private var flower: String = "매발톱"
+
+    override fun init() {
+        super.init()
+        makeFeelingFlowerGuideText()
+        binding.backButton.setOnClickListener {
+            routeToFeelingPage()
+        }
+    }
+
+    private fun routeToFeelingPage() =
+        findNavController().navigate(R.id.action_feelingFlowerDetailFragment_to_feelingFragment)
+
+    private fun makeFeelingFlowerGuideText() {
+        val feelingText = getString(R.string.feeling_guide, feeling)
+        val flowerText = getString(R.string.flower_guide, flower)
+
+        val feelingTextStartIndex = feelingText.indexOf(feeling)
+        val feelingTextEndIndex = feelingTextStartIndex + feeling.length
+
+        val flowerTextStartIndex = flowerText.indexOf(flower)
+        val flowerTextEndIndex = flowerTextStartIndex + flower.length
+
+        val feelingSpannableText = SpannableStringBuilder(feelingText).apply {
+            setSpan(
+                ForegroundColorSpan(requireContext().resources.getColor(R.color.dark_green, null)),
+                feelingTextStartIndex,
+                feelingTextEndIndex,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+            setSpan(
+                StyleSpan(Typeface.BOLD),
+                feelingTextStartIndex,
+                feelingTextEndIndex,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+        }
+
+        val flowerSpannableText = SpannableStringBuilder(flowerText).apply {
+            setSpan(
+                ForegroundColorSpan(requireContext().resources.getColor(R.color.dark_green, null)),
+                flowerTextStartIndex,
+                flowerTextEndIndex,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+            setSpan(
+                StyleSpan(Typeface.BOLD),
+                flowerTextStartIndex,
+                flowerTextEndIndex,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+        }
+
+        binding.feelingFlowerFeelingText.setText(
+            feelingSpannableText,
+            TextView.BufferType.SPANNABLE
+        )
+        binding.feelingFlowerFlowerText.setText(
+            flowerSpannableText,
+            TextView.BufferType.SPANNABLE
+        )
+    }
+}
--- damhwa_client/DamHwa_Android/app/src/main/java/com/example/damhwa_android/ui/feeling/FeelingFragment.kt
+++ damhwa_client/DamHwa_Android/app/src/main/java/com/example/damhwa_android/ui/feeling/FeelingFragment.kt
@@ -1,5 +1,6 @@
 package com.example.damhwa_android.ui.feeling
 
+import androidx.navigation.fragment.findNavController
 import com.example.damhwa_android.R
 import com.example.damhwa_android.base.BaseFragment
 import com.example.damhwa_android.databinding.FragmentFeelingBinding
@@ -7,5 +8,13 @@ import com.example.damhwa_android.databinding.FragmentFeelingBinding
 class FeelingFragment : BaseFragment<FragmentFeelingBinding>(
     R.layout.fragment_feeling
 ) {
+    override fun init() {
+        super.init()
+        binding.changeFlower.setOnClickListener {
+            routeToFlowerFeelingDetail()
+        }
+    }
 
+    private fun routeToFlowerFeelingDetail() =
+        findNavController().navigate(R.id.action_feelingFragment_to_feelingFlowerDetailFragment)
 }
