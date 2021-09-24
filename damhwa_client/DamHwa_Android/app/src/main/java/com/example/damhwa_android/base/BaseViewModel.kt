972336c399a50093117ef2f0dcf270f3a596fc23
Huijiny <jiin20803@gmail.com>
2021-09-24 17:12:17 +0900

    Add Rxjava and baseviewmodel for async network call

---
+++ damhwa_client/DamHwa_Android/app/src/main/java/com/example/damhwa_android/base/BaseViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.damhwa_android.base
+
+import androidx.lifecycle.ViewModel
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.disposables.Disposable
+
+class BaseViewModel: ViewModel() {
+    private val compositeDisposable = CompositeDisposable()
+
+    fun addDisposable(disposable: Disposable) = compositeDisposable.add(disposable)
+
+    override fun onCleared() {
+        compositeDisposable.clear()
+        super.onCleared()
+    }
+
+
+}
