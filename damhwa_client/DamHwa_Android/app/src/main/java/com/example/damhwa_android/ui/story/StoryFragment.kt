c0f0d414856cdab48bf015446752a44c4ebd5cdc
Huijiny <jiin20803@gmail.com>
2021-09-24 11:19:26 +0900

    Add buttons of storyRecFlowerFragment for navigation test

--- damhwa_client/DamHwa_Android/app/src/main/java/com/example/damhwa_android/ui/story/StoryFragment.kt
+++ damhwa_client/DamHwa_Android/app/src/main/java/com/example/damhwa_android/ui/story/StoryFragment.kt
@@ -1,6 +1,7 @@
 package com.example.damhwa_android.ui.story
 
 
+import androidx.navigation.fragment.findNavController
 import com.example.damhwa_android.R
 import com.example.damhwa_android.base.BaseFragment
 import com.example.damhwa_android.databinding.FragmentStoryBinding
@@ -8,5 +9,13 @@ import com.example.damhwa_android.databinding.FragmentStoryBinding
 class StoryFragment : BaseFragment<FragmentStoryBinding>(
     R.layout.fragment_story
 ) {
+    override fun init() {
+        super.init()
+        binding.changeFlower.setOnClickListener {
+            routeToStoryRecFragment()
+        }
+    }
 
+    private fun routeToStoryRecFragment() =
+        findNavController().navigate(R.id.action_storyFragment_to_storyRecFlowerFragment)
 }
