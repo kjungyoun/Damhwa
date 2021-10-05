8fed846bbd64d74aec05b0d5f55b0d7c851c7fe4
kjungyoun <rlawjddbs963@naver.com>
2021-10-05 12:20:51 +0900

    [feat]:History Feature Update (search History by hno)

--- damhwa_server/spring_server/src/main/java/com/ssafy/damhwa/api/service/HistoryService.java
+++ damhwa_server/spring_server/src/main/java/com/ssafy/damhwa/api/service/HistoryService.java
@@ -8,4 +8,5 @@ public interface HistoryService {
     void createMsgHistoryList(int fno, long userno, String msg, String receiver);
     void createStateHistory(int fno, long userno, String state);
     List<History> getHistoryList(long userno);
+    History getHistory(long hno);
 }
