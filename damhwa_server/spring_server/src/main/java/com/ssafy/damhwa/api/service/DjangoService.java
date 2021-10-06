a950ca9373d9d4c2805416e69174f8d63e5dbcad
kjungyoun <rlawjddbs963@naver.com>
2021-10-07 04:51:58 +0900

    [fix]: History Date 수정

--- damhwa_server/spring_server/src/main/java/com/ssafy/damhwa/api/service/DjangoService.java
+++ damhwa_server/spring_server/src/main/java/com/ssafy/damhwa/api/service/DjangoService.java
@@ -1,13 +1,11 @@
 package com.ssafy.damhwa.api.service;
 
-import com.ssafy.damhwa.api.response.FlowerNEmotionRes;
 import com.ssafy.damhwa.db.entity.Flower;
-import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
 public interface DjangoService {
 
     List<Flower> getMsgRecommendFlower(String msg);
-    Flower getStateRecommendFlower (String state);
+    Flower getStateRecommendFlower (String state, long userno);
 }
